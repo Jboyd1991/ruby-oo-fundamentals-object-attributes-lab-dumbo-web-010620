cf58d47f6ca838518ae5e0de6a9aba91ae6520ee
Julianna Boyd <julianna.boyd91@gmail.com>
2020-01-07 17:16:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,17 @@
 class Dog 
-  def initialize(name)
+def name=(name)
     @name = name
   end
- 
+
   def name
     @name
   end
- 
-  def name=(new_name)
-    @name = new_name
-  end
- 
-end
 
-fido = Dog.new("Fido")
-fido.name = "Fido"
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+end
